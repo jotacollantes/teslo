61faf9daa48cf26bf208de234ddd02c22027e540
Juan Collantes <juan@collantes.ec>
2023-02-04 01:55:15 -0500

    cascaron de modelo de mongoose corregido

--- models/Cascaron.ts
+++ models/Cascaron.ts
@@ -1,6 +1,4 @@
 import mongoose, { Schema, model, Model } from "mongoose";
-import {  Interface} from "../interfaces";
-
 const modeloSchema = new Schema({
   name: { type: String, required: true },
  
@@ -9,5 +7,5 @@ const modeloSchema = new Schema({
   },
 {timestamps:true});
 
-const Modelo: Model<Interface>=mongoose.models.Modelo|| model('Modelo',modeloSchema)
+const Modelo: Model<any>=mongoose.models.Modelo|| model('Modelo',modeloSchema)
 export default Modelo
